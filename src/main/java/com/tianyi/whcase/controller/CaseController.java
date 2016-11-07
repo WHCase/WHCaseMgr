e99437090d33d231441a12796b9b094cb062550a
lq <xiaohutuxian5212@163.com>
2016-11-07 09:06:33 +0800

    1

--- src/main/java/com/tianyi/whcase/controller/CaseController.java
+++ src/main/java/com/tianyi/whcase/controller/CaseController.java
@@ -103,7 +103,7 @@ public class CaseController {
 		page = page == 0 ? 1 : page;
 		map.put("pageStart", (page - 1) * rows);
 		map.put("pageSize", rows);
-		ListResult<CaseVM> caseList ;
+		ListResult<CaseVM> caseList = new  ListResult<CaseVM>();
 		if (distributeCase.getCaseType() == 2) {
 			map.put("organId", distributeCase.getOrganId());
 			caseList = caseService.getDistributeCaseByOrganId(map);
