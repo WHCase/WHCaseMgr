82e60f74ea950a4d1d828150220cc7a38f501577
lq <xiaohutuxian5212@163.com>
2016-11-23 15:21:30 +0800

    1

---
+++ src/test/java/com/whcase/test/ycylInterfaceTest.java
@@ -0,0 +1,37 @@
+package com.whcase.test;
+
+import org.junit.Test;
+
+public class ycylInterfaceTest extends BaseTest {
+
+	public static String IP = "121.199.8.150";
+	public static int port = 8080;
+	public static String scope1 = "case";
+	public static String scope2 = "caseAttch";
+	
+	@Test
+	public void AddAttach(){
+		
+	}
+	
+	@Test
+	public void DeleteAttach(){
+		
+	}
+	
+	@Test
+	public void DeleteCase(){
+		
+	}
+	
+	@Test
+	public void UpdateCase(){
+		
+	}
+	
+	@Test
+	public void AddCase(){
+		
+	}
+
+}
