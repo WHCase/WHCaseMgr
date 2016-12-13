0e314cc10d010a12c8ab56ecaa3d0a0fec3ad51e
lq <xiaohutuxian5212@163.com>
2016-12-13 10:27:43 +0800

    1

--- src/main/java/com/tianyi/whcase/controller/XMLController.java
+++ src/main/java/com/tianyi/whcase/controller/XMLController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.tianyi.whcase.model.Case;
+import com.tianyi.whcase.model.Case; 
 import com.tianyi.whcase.service.CaseService;
 
 
